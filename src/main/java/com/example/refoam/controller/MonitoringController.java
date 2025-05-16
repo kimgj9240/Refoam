577d4af4c651d04d871db122e3bb38851511dcc7
lwonbi <lwonbi90@gmail.com>
2025-05-16 11:58:51 +0900

    add monitoring/editn navbar

--- src/main/java/com/example/refoam/controller/MonitoringController.java
+++ src/main/java/com/example/refoam/controller/MonitoringController.java
@@ -1,4 +1,16 @@
 package com.example.refoam.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/monitoring")
 public class MonitoringController {
+    @GetMapping("/main")
+    public String create(){
+        return "monitoring/errorMonitoring";
+    }
 }
