40244a5d98f395009c65c54f094cd8dea0f6c8dc
김건주 <kimgj9240@naver.com>
2025-05-19 12:22:43 +0900

    add OrderRepository, OrderService

---
+++ src/main/java/com/example/refoam/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.example.refoam.repository;
+
+import com.example.refoam.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ src/main/java/com/example/refoam/service/OrderService.java
@@ -0,0 +1,43 @@
+package com.example.refoam.service;
+
+import com.example.refoam.domain.Order;
+import com.example.refoam.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@Slf4j
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final MaterialService materialService;
+
+    // 주문 생성
+    @Transactional
+    public Long save(Order order){
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    // 단건 조회
+    public Optional<Order> findOneOrder(Long orderId){
+        return orderRepository.findById(orderId);
+    }
+    
+    // 전체 주문 조회
+    public List<Order> findOrders(){
+        return orderRepository.findAll();
+    }
+    
+    // 주문 삭제
+    @Transactional
+    public void deleteOrder(Long orderId){
+        orderRepository.deleteById(orderId);
+    }
+}
