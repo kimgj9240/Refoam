3356208510631e66a1e8b29a53caf387f7d6b323
김건주 <kimgj9240@naver.com>
2025-05-20 14:28:30 +0900

    add ProductStandardValue interface

---
+++ src/main/java/com/example/refoam/service/ProductStandardValue.java
@@ -0,0 +1,54 @@
+package com.example.refoam.service;
+
+public interface ProductStandardValue {
+    public static final double MIN_MELT_TEMPERATURE = 105;
+    public static final double MAX_MELT_TEMPERATURE = 108;
+
+    public static final double MIN_MOLD_TEMPERATURE = 80.5;
+
+    public static final double MAX_MOLD_TEMPERATURE = 81.5;
+
+    public static final double MIN_TIME_TO_FILL = 6.8;
+
+    public static final double MAX_TIME_TO_FILL= 11;
+
+    public static final double MIN_PLASTICZING_TIME = 2.78;
+
+    public static final double MAX_PLASTICZING_TIME = 6.61;
+
+    public static final double MIN_CYCLE_TIME = 74.78;
+
+    public static final double MAX_CYCLE_TIME = 75.79;
+
+    public static final double MIN_CLOSING_FORCE = 880;
+
+    public static final double MAX_CLOSING_FORCE = 925;
+
+    public static final double MIN_CLAMPING_FORCE_PEAK = 900;
+
+    public static final double MAX_CLAMPING_FORCE_PEAK = 940;
+
+    public static final double MIN_TORQUE_PEAK= 94.2;
+
+    public static final double MAX_TORQUE_PEAK= 130.3;
+
+    public static final double MIN_TORQUE_MEAN= 76.5;
+
+    public static final double MAX_TORQUE_MEAN= 114.9;
+
+    public static final double MIN_BACK_PRESSURE_PEAK= 144.8;
+
+    public static final double MAX_BACK_PRESSURE_PEAK= 150.5;
+
+    public static final double MIN_INJ_PRESSURE_PEAK= 810;
+
+    public static final double MAX_INJ_PRESSURE_PEAK= 925;
+
+    public static final double MIN_SCREW_POS_END_HOLD= 8.6;
+
+    public static final double MAX_SCREW_POS_END_HOLD= 9.0;
+
+    public static final double MIN_SHOT_VOLUME= 18.51;
+
+    public static final double MAX_SHOT_VOLUME= 19.23;
+}
