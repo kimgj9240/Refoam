ea27cf39ec2b900724d9424e63b3720c78740b32
lwonbi <lwonbi90@gmail.com>
2025-05-20 15:47:33 +0900

    mod ProductStandardValue

--- src/main/java/com/example/refoam/service/ProductStandardValue.java
+++ src/main/java/com/example/refoam/service/ProductStandardValue.java
@@ -1,54 +1,63 @@
 package com.example.refoam.service;
 
-public interface ProductStandardValue {
-    public static final double MIN_MELT_TEMPERATURE = 105;
-    public static final double MAX_MELT_TEMPERATURE = 108;
+import java.util.Random;
+interface ProductStandardConst {
+    final double MIN_MELT_TEMPERATURE = 105.900;
+    final double MAX_MELT_TEMPERATURE = 107.989;
 
-    public static final double MIN_MOLD_TEMPERATURE = 80.5;
+    final double MIN_MOLD_TEMPERATURE = 81.012;
 
-    public static final double MAX_MOLD_TEMPERATURE = 81.5;
+    final double MAX_MOLD_TEMPERATURE = 81.918;
 
-    public static final double MIN_TIME_TO_FILL = 6.8;
+    final double MIN_TIME_TO_FILL = 6.206;
 
-    public static final double MAX_TIME_TO_FILL= 11;
+    final double MAX_TIME_TO_FILL= 7.177;
 
-    public static final double MIN_PLASTICZING_TIME = 2.78;
+    public static final double MIN_PLASTICIZING_TIME = 2.77;
 
-    public static final double MAX_PLASTICZING_TIME = 6.61;
+    public static final double MAX_PLASTICIZING_TIME = 4.14;
 
     public static final double MIN_CYCLE_TIME = 74.78;
 
-    public static final double MAX_CYCLE_TIME = 75.79;
+    public static final double MAX_CYCLE_TIME = 75.81;
 
-    public static final double MIN_CLOSING_FORCE = 880;
+    public static final double MIN_CLOSING_FORCE = 883.3;
 
-    public static final double MAX_CLOSING_FORCE = 925;
+    public static final double MAX_CLOSING_FORCE = 921.9;
 
-    public static final double MIN_CLAMPING_FORCE_PEAK = 900;
+    public static final double MIN_CLAMPING_FORCE_PEAK = 892.7;
 
-    public static final double MAX_CLAMPING_FORCE_PEAK = 940;
+    public static final double MAX_CLAMPING_FORCE_PEAK = 936.6;
 
-    public static final double MIN_TORQUE_PEAK= 94.2;
+    public static final double MIN_TORQUE_PEAK= 108.6;
 
-    public static final double MAX_TORQUE_PEAK= 130.3;
+    public static final double MAX_TORQUE_PEAK= 126.7;
 
-    public static final double MIN_TORQUE_MEAN= 76.5;
+    public static final double MIN_TORQUE_MEAN= 80.2;
 
-    public static final double MAX_TORQUE_MEAN= 114.9;
+    public static final double MAX_TORQUE_MEAN= 109.7;
 
-    public static final double MIN_BACK_PRESSURE_PEAK= 144.8;
+    public static final double MIN_BACK_PRESSURE_PEAK= 144.801;
 
-    public static final double MAX_BACK_PRESSURE_PEAK= 150.5;
+    public static final double MAX_BACK_PRESSURE_PEAK= 150.206;
 
-    public static final double MIN_INJ_PRESSURE_PEAK= 810;
+    public static final double MIN_INJ_PRESSURE_PEAK= 846.65;
 
-    public static final double MAX_INJ_PRESSURE_PEAK= 925;
+    public static final double MAX_INJ_PRESSURE_PEAK= 925.71;
 
-    public static final double MIN_SCREW_POS_END_HOLD= 8.6;
+    public static final double MIN_SCREW_POS_END_HOLD= 8.441;
 
-    public static final double MAX_SCREW_POS_END_HOLD= 9.0;
+    public static final double MAX_SCREW_POS_END_HOLD= 9.037;
 
-    public static final double MIN_SHOT_VOLUME= 18.51;
+    public static final double MIN_SHOT_VOLUME= 18.511;
 
-    public static final double MAX_SHOT_VOLUME= 19.23;
+    public static final double MAX_SHOT_VOLUME= 19.229;
+
 }
+public class ProductStandardValue implements ProductStandardConst{
+    public double getRandomValue(double min, double max){
+        Random random = new Random();
+        return random.nextDouble(min,max);
+    }
+}
+
