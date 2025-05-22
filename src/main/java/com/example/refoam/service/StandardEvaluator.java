574d5d833368c6c5df44dffae8b7ceaf6e758d7e
김건주 <kimgj9240@naver.com>
2025-05-22 14:56:41 +0900

    mod evaluator, List, enum Label

--- src/main/java/com/example/refoam/service/StandardEvaluator.java
+++ src/main/java/com/example/refoam/service/StandardEvaluator.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Component;
 public class StandardEvaluator {
     public ProductLabel evaluate(double screw, double mold, double injpress, double fill) {
         // 특정 값에 따른 라벨 부여
-        if (screw > 8.835) return ProductLabel.ERR_POS_021;
-        if (injpress > 915) return ProductLabel.ERR_PRS_021;
-        if ((mold > 81.5 || mold < 80.5)
-                && fill > 6.864) return ProductLabel.ERR_TEMP_021;
+        if (screw > 8.835 && Math.random() > 0.777) return ProductLabel.ERR_POS_01;
+        if (injpress > 915 && Math.random() > 0.777) return ProductLabel.ERR_PRS_01;
+        if (((mold < 81.5 || mold > 80.5) && fill > 6.864) && Math.random() > 0.777) return ProductLabel.ERR_TEMP_01;
+        if(fill > 11 && Math.random() > 0.777) return ProductLabel.ERR_TIME_01;
 
         return ProductLabel.OK;
     }
