292e8749b6e022e2a2be9519e1a88f8c994d594a
lwonbi <lwonbi90@gmail.com>
2025-05-26 17:30:01 +0900

    add errorCheckSmtpSend

--- src/main/java/com/example/refoam/repository/OrderRepository.java
+++ src/main/java/com/example/refoam/repository/OrderRepository.java
@@ -2,10 +2,13 @@ package com.example.refoam.repository;
 
 import com.example.refoam.domain.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
 
     List<Orders> findAllByOrderStateAndStatisticsIntervalCheck(String orderState,boolean statisticsIntervalCheck);// 준비 중, 배합완료, 배합실패, 공정완료, 진행 중
+    @Query("SELECT o FROM Orders o JOIN FETCH o.employee WHERE o.orderState = :orderState AND o.statisticsIntervalCheck = :statisticsIntervalCheck AND o.smtpCheck = :smtpCheck")
+    List<Orders> findAllByOrderStateAndStatisticsIntervalCheckAndSmtpCheck(String orderState,boolean statisticsIntervalCheck,boolean smtpCheck);// 준비 중, 배합완료, 배합실패, 공정완료, 진행 중
 }
